3c5ba82c64b7714f33c3f6290582816df7cdf456
tsb46 <tsb46@miami.edu>
2023-01-19 23:04:17 -0500

    modify file paths in config attempt2

--- create_config.py
+++ create_config.py
@@ -8,10 +8,10 @@ os.makedirs('data/derivatives', exist_ok=True)
 
 # Set config variables
 config_mod_dir = 'preprocess/pipeline_config/modified'
-data_base_dir = '/data'
-output_dir = '/data/derivatives'
+data_base_dir = 'data'
+output_dir = 'data/derivatives'
 fwhm = ['[3', '3', '3]']
-scratch_dir='/data/tmp'
+scratch_dir='data/tmp'
 
 ini_fps = glob('preprocess/pipeline_config/original/*.ini')
 
