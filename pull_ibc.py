044a79708d71e4674d07b6448a39490dacace621
tsb46 <tsb46@miami.edu>
2023-01-31 12:11:09 -0500

    download preprocessed data from ebrains

--- pull_ibc.py
+++ pull_ibc.py
@@ -1,16 +1,42 @@
 import os
-import openneuro as on
+
+from config import ebrains_user, ebrains_pw
+from itertools import repeat
+from ebrains_drive import BucketApiClient
+from multiprocessing import Pool
+from pathlib import Path
 
 
-ds_num = 'ds002685'
-tag_num = '1.3.1'
-
-os.makedirs('data', exist_ok=True)
-on.download(dataset=ds_num, tag=tag_num, target_dir='data')
-
-
+def ebrains_connect(ebrains_user, ebrains_pw):
+	client = BucketApiClient(ebrains_user, ebrains_pw)
+	# Preprocess IBC Database
+	bucket = client.buckets.get_dataset("3ca4f5a1-647b-4829-8107-588a699763c1")
+	return bucket
 
 
+def download_file(fp, output_dir):
+	fp_path = Path(fp.name)
+	fp_out = Path(output_dir, *fp_path.parts[1:])
+	if os.path.isfile(fp_out):
+		return
+	else:
+		print(fp_out)
+		os.makedirs(os.path.dirname(fp_out), exist_ok=True)
+		open(fp_out, 'wb').write(fp.get_content())
 
+
+
+
+if __name__ == '__main__':
+
+	output_dir = 'data'
+
+	bucket = ebrains_connect(ebrains_user, ebrains_pw)
+
+	# Pull all files in database
+	bucket_files = bucket.ls()
+
+	pool = Pool(processes=3)
+	pool.starmap(download_file, zip(bucket_files, repeat(output_dir)))
 
 
